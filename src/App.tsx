2c75bb380c802d22a7bb2d1f844f014f9fbda99f
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-04-03 13:22:37 +0300

    firstpart first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from './Components/Container';
+import { MyList } from './Components/List';
+import { Search } from './Components/Search';
 
 function App() {
+  const [value, setValue] = React.useState<string | null>('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Search 
+        value={value}
+        setValue={setValue}
+      />
+      <MyList 
+        value={value}
+      />
+    </Container>
   );
 }
 
